c84641713eeb4caf61bc6ac15482fb120e3d4e0a
Olumide Ogundare <olumide.ogundare@interswitchgroup.com>
2019-12-27 07:11:07 +0100

    Configured app.ts

---
+++ lib/app.ts
@@ -0,0 +1,31 @@
+import {
+  BotFrameworkAdapter,
+  MemoryStorage,
+  ConversationState
+} from "botbuilder";
+import * as restify from "restify";
+import { ConfState } from "./types";
+
+let server = restify.createServer();
+server.listen(process.env.port || process.env.port || 3978, () => {
+  console.log(`${server.name} listening on ${server.url}`);
+});
+
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+const conversationState = new ConversationState<ConfState>(new MemoryStorage());
+adapter.use(conversationState);
+
+server.post("/api/messages", (req, res) => {
+  adapter.processActivity(req, res, async context => {
+    if (context.activity.type === "message") {
+      const state = conversationState.get(context);
+      await context.sendActivity(`You said ${context.activity.text}`);
+    } else {
+      await context.sendActivity(`${context.activity.type} event detected`);
+    }
+  });
+});
